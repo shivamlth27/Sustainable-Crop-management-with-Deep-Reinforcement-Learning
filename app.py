f4ddefef60026eb73d6f3201563d479f7ebef678
shivamlth27 <shivam16103@gmail.com>
2024-09-16 13:23:14 +0530

    v2

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
+import matplotlib.pyplot as plt
+from crop_model import predict
 from flask import Flask, render_template, request
-from flask_cors import CORS 
+from flask_cors import CORS
 from fr import train_model
 import matplotlib
 from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
-
+CORS(app)
 #######################################################################################################################
 
 
-from crop_model import predict
 @app.route('/predict_crop', methods=['POST'])
 def predict_crop():
     if request.method == 'POST':
         data = request.get_json()
         prediction = predict(data)
-        return jsonify({'prediction': prediction[0]})  # Assuming prediction is a single value
+        # Assuming prediction is a single value
+        return jsonify({'prediction': prediction[0]})
 
 #####################################################################################################################################
 
+
 # Set the backend to a non-GUI backend before importing pyplot
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 
-crop_mapping = {'Maize': 1, 'Paddy': 0, 'Sugarcane': 2, 'Cotton': 3, 'Tobacco': 4, 'Barley': 5, 'Wheat': 6, 'Millets': 7}
+crop_mapping = {'Maize': 1, 'Paddy': 0, 'Sugarcane': 2,
+                'Cotton': 3, 'Tobacco': 4, 'Barley': 5, 'Wheat': 6, 'Millets': 7}
 soil_mapping = {'Sandy': 0, 'Loamy': 1, 'Black': 2, 'Red': 3, 'Clayey': 4}
 
 
@@ -37,21 +40,26 @@ def index():
 def crop_pred():
     return render_template('crop_pred.html')
 
+
 @app.route('/yield_prediction')
 def yield_prediction():
     return render_template('yield_pred.html')
+
 
 @app.route('/crop_recommend')
 def crop_recommendation():
     return render_template('crop_recommend.html')
 
+
 @app.route('/rainfall_prediction')
 def rainfall_prediction():
     return render_template('rain_pred.html')
 
+
 @app.route('/fertilizer_recommend')
 def fertilizer_recommend():
     return render_template('fertilizer_recommend.html')
+
 
 @app.route('/submit_form', methods=['POST'])
 def submit_form():
@@ -67,12 +75,12 @@ def submit_form():
             'phosphorous': int(request.form.get('p')),
             'potassium': int(request.form.get('k'))
         }
-        
+
         # Print the form data
         print(form_data)
 
         # Pass the form data to the train_model function
-        result= train_model(form_data)
+        result = train_model(form_data)
         print(result[0][0])
         # Return a response
         return jsonify({'prediction': result[0][0]})
@@ -80,5 +88,3 @@ def submit_form():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
-
-
